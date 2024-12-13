2b6368450eee6cd0c7bdd2992b717308fa2627b4
ecblandy <Viny19381@hotmail.com>
2024-12-13 12:12:08 -0300

    Middlewares: Aprendendo a como usar middlewares no Next.

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+export function middleware(request: NextRequest) {
+  const authenticated = true;
+  if (request.nextUrl.pathname.startsWith("/dashbord") && !authenticated) {
+    console.log("Acesso negado");
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
+}
