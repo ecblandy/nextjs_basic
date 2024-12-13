2f392bade5dbc4e57b47a40efe91e9b52b4c6824
ecblandy <Viny19381@hotmail.com>
2024-12-13 03:51:38 -0300

    Server actions: Da pra usar com uma API/Banco de dados e consigo chamar dentro do proprio componente. Também consigo acessar um input.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,5 @@
 // Server components --> Clients components
-import Button from "@/components/button";
+// import Button from "@/components/button";
 interface PostsProps {
   id: number;
   title: string;
@@ -14,12 +14,37 @@ interface ResponseProps {
 export default async function Posts() {
   const response = await fetch("https://dummyjson.com/posts");
   const data: ResponseProps = await response.json();
-  console.log(data);
+
+  async function handleFetchPosts() {
+    "use server";
+    console.log("CLICOU");
+  }
+
+  // Utilizando a action eu consigo obter dados do form sem precisar da reatividade como onclick do lado client.
+  async function handleSearchUsers(formData: FormData) {
+    "use server";
+    const userId = formData.get("userId");
+    const response = await fetch(`https://dummyjson.com/posts/user/${userId}`);
+    const data: ResponseProps = await response.json();
+
+    console.log(data);
+  }
 
   return (
     <>
       <h1 className="text-center mt-5 mb-2 font-bold">Todos os posts</h1>
-      <Button />
+      <button onClick={handleFetchPosts}>CLIQUE</button>
+
+      <form action={handleSearchUsers} className="flex gap-2 my-4">
+        <input
+          type="text"
+          placeholder="ID do usuário"
+          className="border border-gray-200 p-2"
+          name="userId"
+        />
+        <button className="bg-blue-500 text-white p-2">Buscar usuario</button>
+      </form>
+      {/* <Button /> */}
       <div className="flex flex-col gap-4">
         {data.posts.map((post) => (
           <div key={post.id} className="bg-gray-200 p-4 mx-2 rounded-md">
